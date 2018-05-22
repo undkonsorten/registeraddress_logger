35e8b6386d5636933b59aa0f5b9afbc604705d8a
eike.starkmann <eike.starkmann@undkonsorten.com>
2018-05-22 12:02:14 +0200

    [ADD]: increase version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,11 +21,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-8.7.99',
-            'registeraddress' => '1.0.6-1.0.99'
+            'registeraddress' => '1.0.13-1.0.99'
         ],
         'conflicts' => [],
         'suggests' => [],
